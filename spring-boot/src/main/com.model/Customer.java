87a072d671c8e8b31c8231fe6ce189ba9556bfe7
unknown <3suraj1@gmail.com>
2023-02-03 14:47:36 +0530

    Suraj Created Customer Model Class.

---
+++ spring-boot/src/main/com.model/Customer.java
@@ -0,0 +1,247 @@
+package com.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Customer {
+
+    @Id@GeneratedValue
+    private int custId;
+	private String custName;
+	private String email;
+	private String gender;
+	private String phoneNumber;
+	private String password;
+    private String type;
+    private String address;
+    private int pincode;
+    private String city;
+    private String state;
+    private String country;
+    private int coins;
+
+
+    public Customer() {
+        super();
+    }
+
+
+    public Customer(int custId, String custName, String email, String gender, String phoneNumber, String password,
+            String type, String address, int pincode, String city, String state, String country, int coins) {
+        this.custId = custId;
+        this.custName = custName;
+        this.email = email;
+        this.gender = gender;
+        this.phoneNumber = phoneNumber;
+        this.password = password;
+        this.type = type;
+        this.address = address;
+        this.pincode = pincode;
+        this.city = city;
+        this.state = state;
+        this.country = country;
+        this.coins = coins;
+    }
+
+    
+
+    
+
+    
+
+    /**
+     * @return int return the custId
+     */
+    public int getCustId() {
+        return custId;
+    }
+
+    /**
+     * @param custId the custId to set
+     */
+    public void setCustId(int custId) {
+        this.custId = custId;
+    }
+
+    /**
+     * @return String return the custName
+     */
+    public String getCustName() {
+        return custName;
+    }
+
+    /**
+     * @param custName the custName to set
+     */
+    public void setCustName(String custName) {
+        this.custName = custName;
+    }
+
+    /**
+     * @return String return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return String return the gender
+     */
+    public String getGender() {
+        return gender;
+    }
+
+    /**
+     * @param gender the gender to set
+     */
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    /**
+     * @return String return the phoneNumber
+     */
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    /**
+     * @param phoneNumber the phoneNumber to set
+     */
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    /**
+     * @return String return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return String return the type
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * @param type the type to set
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    /**
+     * @return String return the address
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * @param address the address to set
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /**
+     * @return int return the pincode
+     */
+    public int getPincode() {
+        return pincode;
+    }
+
+    /**
+     * @param pincode the pincode to set
+     */
+    public void setPincode(int pincode) {
+        this.pincode = pincode;
+    }
+
+    /**
+     * @return String return the city
+     */
+    public String getCity() {
+        return city;
+    }
+
+    /**
+     * @param city the city to set
+     */
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    /**
+     * @return String return the state
+     */
+    public String getState() {
+        return state;
+    }
+
+    /**
+     * @param state the state to set
+     */
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    /**
+     * @return String return the country
+     */
+    public String getCountry() {
+        return country;
+    }
+
+    /**
+     * @param country the country to set
+     */
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    /**
+     * @return int return the coins
+     */
+    public int getCoins() {
+        return coins;
+    }
+
+    /**
+     * @param coins the coins to set
+     */
+    public void setCoins(int coins) {
+        this.coins = coins;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Customer [custId=" + custId + ", custName=" + custName + ", email=" + email + ", gender=" + gender
+                + ", phoneNumber=" + phoneNumber + ", password=" + password + ", type=" + type + ", address=" + address
+                + ", pincode=" + pincode + ", city=" + city + ", state=" + state + ", country=" + country + ", coins="
+                + coins + "]";
+    }
+
+    
+
+}
